9785ea55249e42e3e035741f8f12fd4d5ec57c60
Logan Wang <loganxwang@hotmail.com>
2018-09-26 11:18:41 -0700

    iterate over object's properties

--- afternoonDrills.js
+++ afternoonDrills.js
@@ -13,3 +13,16 @@ const loaf = {
 console.log(loaf.flour);
 console.log(loaf.water);
 console.log(loaf.hydration());
+
+//Iterate over an object's properties
+const foobar = {
+  foo: 'foo',
+  bar: 'bar',
+  fum: 'fum',
+  quux: 'quux',
+  spam: 'spam'
+};
+
+for (let property in foobar){
+  console.log(`${property}: ${foobar[property]}`);
+}
