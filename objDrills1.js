d73c2118a8b89f7f959250f29899a8252325304e
jsantiag <jsantiag@wellesley.edu>
2018-09-26 11:00:41 -0500

    updateObjdrill

--- objDrills1.js
+++ objDrills1.js
@@ -90,32 +90,87 @@ function updateObject(obj) {
    
 */
 
-(function testUpdateObject() {
-  var oldObj = {
-    cats: 'cats',
-    dogs: 'dogs',
-  };
-  var newObj = updateObject(oldObj);
-  if (typeof newObj !== 'object') { 
-    console.error('ERROR: `createMyObject` must return an object');
-    return false
-  }
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
-    if (!(key in newObj)) {
-      console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
-      return false;
-    }
-  });
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
-    if (newObj[key] !== key) {
-      console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
-      return false;
-    }
-  });
-  if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
-    console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
-    return false;
-  }
-  console.log('SUCCESS: `updateObject` works correctly!');
+// (function testUpdateObject() {
+//   var oldObj = {
+//     cats: 'cats',
+//     dogs: 'dogs',
+//   };
+//   var newObj = updateObject(oldObj);
+//   if (typeof newObj !== 'object') { 
+//     console.error('ERROR: `createMyObject` must return an object');
+//     return false
+//   }
+//   ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+//     if (!(key in newObj)) {
+//       console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
+//       return false;
+//     }
+//   });
+//   ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+//     if (newObj[key] !== key) {
+//       console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
+//       return false;
+//     }
+//   });
+//   if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
+//     console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
+//     return false;
+//   }
+//   console.log('SUCCESS: `updateObject` works correctly!');
   
-})();
+// })();
+
+
+// Modify personMaker.fullName to be a function that uses self-reference (via this) in order to return the firstName and lastName properties separated by a space.
+
+// So, for instance, if firstName was 'Jane' and lastName was 'Doe', fullName() should return 'Jane Doe'.
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function(){
+          return `${this.firstName} ${this.lastName}`; 
+    },
+   }; return person;
+  }
+  
+  /* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     
+     Nothing to see here!
+     
+  */
+  
+  (function testPersonMaker() {
+    var person = personMaker();
+    if (typeof person !== 'object') {
+      console.error('ERROR: `personMaker` must return an object');
+      return false;
+    }
+    if (typeof person.fullName !== 'function') {
+      console.error('ERROR: `fullName` must be a method');
+      return false;
+    }
+    if (person.fullName() !== 'Paul Jones') {
+      console.error(
+        'ERROR: The value for `fullName` should be "Paul Jones" but was ' +
+          person.fullName()
+      );
+      return false;
+    }
+    person.firstName = 'Lisa';
+    person.lastName = 'Simpson';
+    if (person.fullName() !== 'Lisa Simpson') {
+      console.error(
+        '`personMaker` is not using self reference correctly. ' +
+          'When firstName set to "Lisa" and lastName set to "Simpson", ' +
+          'should return "Lisa Simpson" but returned ' +
+          person.fullName()
+      );
+    }
+    console.log('SUCCESS: `updateObject` works correctly!');
+  })();
+  
