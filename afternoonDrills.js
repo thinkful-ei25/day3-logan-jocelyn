de740ebe367d0901af8f0ac6d4d28ba26ec8162c
jsantiag <jsantiag@wellesley.edu>
2018-09-26 19:56:39 -0500

    Bonus II

--- afternoonDrills.js
+++ afternoonDrills.js
@@ -155,22 +155,56 @@ function findOne (arr, query){
   for (const hero of arr){
     let match = true; 
     
-      for(const key in query){
+    for(const key in query){
 
+      if (query[key] !== hero[key]){
+        match = false; 
+        break;
+      }
+    } 
+      
+    if(!match) {
+      continue;
+    }
+    return hero; 
+  }
+  return null; 
+
+}
+
+//console.log(findOne(HEROES,  { id: 1, name: 'Captain America', squad: 'Avengers' }));
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]  
+  },
+  findOne: function (query){
+    for (const hero of this.store.heroes){
+      let match = true; 
+      
+      for(const key in query){
+  
         if (query[key] !== hero[key]){
           match = false; 
           break;
         }
       } 
-      
+        
       if(!match) {
         continue;
+      }
+      return hero; 
     }
-     return hero; 
-  }
     return null; 
-
-}
-
-console.log(findOne(HEROES,  { id: 1, name: 'Captain America', squad: 'Avengers' },
-));
+  
+  }
+};
+//console.log(Database.findOne({id:2}));
